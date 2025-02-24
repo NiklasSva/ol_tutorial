6823f6321e3c9e676f27b54d0bd3ae2340063863
Niklas <6541562+NiklasSva@users.noreply.github.com>
2025-02-24 10:12:40 +0100

    resume work after absence
    Cleaning up the code with help from copilot

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 import './style.css';
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import TileWMS from 'ol/source/TileWMS.js';
-import TileLayer 'ol/layer/Tile.js';
-import TileGrid 'ol/tilegrid/TileGrid.js';
+import TileLayer from 'ol/layer/Tile.js';
+import TileGrid from 'ol/tilegrid/TileGrid.js';
 import {get as getProjection} from 'ol/proj.js';
 import {getWidth} from 'ol/extent.js';
 
-const projExtent = getProjection('ESPG:3857').getExtent();
+const projExtent = getProjection('EPSG:3857').getExtent();
 const startResolution = getWidth(projExtent) / 256;
 const resolutions = new Array(22);
 for (let i = 0, ii = resolutions.length; i < ii; ++i)
